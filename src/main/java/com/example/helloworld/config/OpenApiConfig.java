5db44d082452e76b8b641f99ada7e4b24d60fa11
sudarshan-uprety <mail@sudarshan-uprety.com.np>
2026-01-26 19:24:16 +0545

    testing scalable database

--- src/main/java/com/example/helloworld/config/OpenApiConfig.java
+++ src/main/java/com/example/helloworld/config/OpenApiConfig.java
@@ -3,15 +3,26 @@ package com.example.helloworld.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server prodServer = new Server();
+        prodServer.setUrl("https://esewaprod.sudarshan-uprety.com.np");
+        prodServer.setDescription("Production Server");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local Server");
+
         return new OpenAPI()
+                .servers(List.of(prodServer, localServer))
                 .info(new Info()
                         .title("eSewa Application API")
                         .version("1.0")
